15d6a2e480486adcd376f800bb91f376a4fbdce0
smalaca <ab13.krakow@gmail.com>
2021-08-07 10:37:53 +0200

    REFACT - add identifier to TeamNotFoundException

--- src/main/java/com/smalaca/taskamanager/exception/TeamNotFoundException.java
+++ src/main/java/com/smalaca/taskamanager/exception/TeamNotFoundException.java
@@ -1,4 +1,14 @@
 package com.smalaca.taskamanager.exception;
 
 public class TeamNotFoundException extends RuntimeException {
+    private Long id;
+
+    public TeamNotFoundException(Long id) {
+        this();
+        this.id = id;
+    }
+
+    public TeamNotFoundException() {
+        super();
+    }
 }
