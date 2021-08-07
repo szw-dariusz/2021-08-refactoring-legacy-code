1dafabc9210f9c00a9e80316caf051339ce0d36f
Dariusz Szwarc <dszwarc@media4u.pl>
2021-08-07 16:06:35 +0200

    Refactor: Extract item processing to state

---
+++ src/main/java/com/smalaca/taskamanager/processor/ReleasedToDoItem.java
@@ -0,0 +1,21 @@
+package com.smalaca.taskamanager.processor;
+
+import com.smalaca.taskamanager.events.ToDoItemReleasedEvent;
+import com.smalaca.taskamanager.model.interfaces.ToDoItem;
+import com.smalaca.taskamanager.registry.EventsRegistry;
+
+class ReleasedToDoItem implements ToDoItemState {
+
+	private final EventsRegistry eventsRegistry;
+
+	ReleasedToDoItem(EventsRegistry eventsRegistry) {
+		this.eventsRegistry = eventsRegistry;
+	}
+
+	@Override
+	public void process(ToDoItem toDoItem) {
+		ToDoItemReleasedEvent event = new ToDoItemReleasedEvent();
+		event.setToDoItemId(toDoItem.getId());
+		eventsRegistry.publish(event);
+	}
+}
---
+++ src/main/java/com/smalaca/taskamanager/processor/ToDoItemState.java
@@ -0,0 +1,8 @@
+package com.smalaca.taskamanager.processor;
+
+import com.smalaca.taskamanager.model.interfaces.ToDoItem;
+
+interface ToDoItemState {
+
+	void process(ToDoItem toDoItem);
+}
